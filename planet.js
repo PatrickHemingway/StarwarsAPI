4100c9477f6312b656b8494aff0a79e3c21ea590
wasadmin <wasadmin@example.com>
2025-06-23 14:59:37 -0400

    Add rest of stuff to planets page

--- planet.js
+++ planet.js
@@ -22,5 +22,31 @@ async function fetchPlanet(id) {
     document.getElementById('gravity').textContent = planet.gravity;
     document.getElementById('population').textContent = planet.population;
 
+    // Fetch and render films and residents
+    await renderFilms(planet.films);
+    await renderResidents(planet.residents);
+
     console.log(url, planet);
+}
+
+async function renderFilms(filmUrls) {
+    const filmsSection = document.getElementById('films');
+    const filmsLis = await Promise.all(filmUrls.map(async url => {
+        const film = await fetch(url).then(res => res.json());
+        const filmUrl = new URL(film.url);
+        const filmId = filmUrl.pathname.split('/').filter(Boolean).pop();
+        return `<li><a href="/films/film.html?id=${filmId}">${film.title}</a></li>`;
+    }));
+    filmsSection.innerHTML = `<h2>Films</h2><ul>${filmsLis.join("")}</ul>`;
+}
+
+async function renderResidents(residentUrls) {
+    const residentsSection = document.getElementById('residents');
+    const residentsLis = await Promise.all(residentUrls.map(async url => {
+        const resident = await fetch(url).then(res => res.json());
+        const residentUrl = new URL(resident.url);
+        const residentId = residentUrl.pathname.split('/').filter(Boolean).pop();
+        return `<li><a href="/character.html?id=${residentId}">${resident.name}</a></li>`;
+    }));
+    residentsSection.innerHTML = `<h2>Residents</h2><ul>${residentsLis.join("")}</ul>`;
 }
